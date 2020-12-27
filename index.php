d2e36720333873a9d30e7f4c59686f55ba76da5d
victor <victor8titov@yandex.ru>
2020-12-27 20:50:26 +0300

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>test site</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+    <!--[if lt IE 7]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <h1>test page</h1>
+    <p>Labore ea deserunt amet ullamco sit dolore aliqua nisi dolor sint qui. Eiusmod deserunt sint culpa ad labore nulla. Aliqua pariatur commodo deserunt id est exercitation do est do culpa non officia. Incididunt est cillum laboris nulla sit id Lorem incididunt consequat cupidatat anim. Nisi nisi laboris enim proident veniam tempor consequat nulla aute ad sit consequat veniam dolore. Sunt enim sit nisi adipisicing labore duis id cupidatat nulla exercitation irure. Voluptate excepteur laborum deserunt minim esse ipsum commodo.</P>
+    <script src="" async defer></script>
+  </body>
+</html>
